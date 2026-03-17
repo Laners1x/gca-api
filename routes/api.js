f5c20961db953dcc5ddd4ba8236c6b5e54f8ad2a
Lanre <lanreakin242@gmail.com>
2026-03-17 14:53:41 +0000

    adding webhook for fine removal

--- routes/api.js
+++ routes/api.js
@@ -120,12 +120,61 @@ router.post('/removeFine/:roblox_id/:fine_id', async (req, res) => {
 
     await player.save()
 
+    const payload = {
+        embeds: [
+            {
+            title: `💸 Fine ${difference > 0 ? "Partially " : ""}Bailed`,
+            color: 0x00BFFF,
+            fields: [
+                {
+                name: "User",
+                    value: `<@${player.discordId}>`,
+                inline: true
+                },
+                {
+                name: "By",
+                value: `Donation Center (In Game)`,
+                inline: true
+                },
+                {
+                name: "Reason",
+                value: removing.reason || "No reason provided",
+                inline: false
+                },
+                {
+                name: `Amount${difference > 0 ? " Bailed" : ""}`,
+                value: `${difference > 0 ? difference : removing.amount} robux`,
+                inline: true
+                }
+            ],
+            timestamp: new Date().toISOString()
+            }
+        ]
+        };
+
+    let webhook_success = false
+    try {
+        const res = await fetch(process.env.WEBHOOK_URL, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(payload)
+        });
+        webhook_success = true
+    }
+
+    catch (e){
+        webhook_success = false
+        console.log(e)
+    }
+
     if (difference > 0){
-        return res.status(200).json({'msg': `Success! Fine partially bailed for ${amount_to_remove}.`, 'success': true})
+
+
+        return res.status(200).json({'msg': `Success! Fine partially bailed for ${amount_to_remove} Robux.`, 'success': true, 'webhook_success': webhook_success})
     }
 
     else {
-        return res.status(200).json({'msg': `Success! Fine with ${removing.reason} for ${removing.reason} for Roblox ID: ${player.robloxId} has been removed.`, 'success': true})
+        return res.status(200).json({'msg': `Success! Fine with ${removing.reason} for ${removing.amount} Robux (Roblox ID: ${player.robloxId}) has been removed.`, 'success': true, 'webhook_success': webhook_success})
     }
 })
 
